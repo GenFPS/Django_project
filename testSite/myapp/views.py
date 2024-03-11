d815f330d5c47c56d7300dea436f7d6d147103f6
Сергей Федотов <fedser@lan.rarus.ru>
2024-03-11 14:37:39 +0300

    создание класса Product в myapp/models.py  для генерации таблицы Product в бд Sqlite3, подключение model.py к  testSite/settings.py и testSite/urls.py, миграция таблицы (myapp/migrations/0001_inital.py)

--- testSite/myapp/views.py
+++ testSite/myapp/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def response(request: str) -> HttpResponse:
+def response(request) -> HttpResponse:
     return HttpResponse('Here\'s your response\n'
                         '\nGlad to see you here! =)')
 
 
-def contacts(request: str) -> HttpResponse:
+def contacts(request) -> HttpResponse:
     return HttpResponse('<h1>Наши контакты</h1>')
+
+def items(requests) -> HttpResponse:
+    items = ['Iphone', 'Samsung', 'Huawie']
+    return HttpResponse(items[0])
--- testSite/testSite/urls.py
+++ testSite/testSite/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     # http://127.0.0.1:8000/greeting/
     path('greeting/', views.response),
     # http://127.0.0.1:8000/contacts/
-    path('contacts/', views.contacts)
+    path('contacts/', views.contacts),
+    # http://127.0.0.1:8000/items/
+    path('items/', views.items)
 ]
