bb57100e971ca85b465bd4aa82b6ec1428166e75
Сергей Федотов <fedser@lan.rarus.ru>
2024-03-11 18:42:32 +0300

    добавил относительные переменные пути для более удобной и гибкой навигации по проекту

--- testSite/testSite/urls.py
+++ testSite/testSite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from myapp import views
 
+app_name = "myapp"
 
 urlpatterns = [
     # http://127.0.0.1:8000/admin/
@@ -17,5 +18,6 @@ urlpatterns = [
     path('items/', views.items),
 
     #http://127.0.0.1:8000/items/<my_id>/
-    path('items/<int:my_id>/', views.details_id)
+    path('items/<int:my_id>/', views.details_id, name='detailItem')     # 3-ий параметр - позволяет использовать
+    # пространство имен (в index.html: <a href="{% url 'detailItem' item.id %}">)
 ]
