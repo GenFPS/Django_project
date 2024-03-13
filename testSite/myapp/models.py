b598ef2770cb3dc192a5ae48cbd85c1d93910c1a
Сергей Федотов <fedser@lan.rarus.ru>
2024-03-13 16:34:55 +0300

    Добавил поле images в таблицу данных Product. Миграция/обновленние, загрузка фотографий .jpg через Django admin. Импорт фотографий в templates/index.html

--- testSite/myapp/models.py
+++ testSite/myapp/models.py
@@ -1,4 +1,4 @@
-from django.db.models import Model, CharField, IntegerField
+from django.db.models import Model, CharField, IntegerField, ImageField
 
 
 # Создаем класс Product для таблицы Product в бд Sqlite3
@@ -6,6 +6,7 @@ class Product(Model):
     name = CharField(max_length=100)
     price = IntegerField()
     description = CharField(max_length=200)
+    images = ImageField(blank=True)
 
     # Вместо product object будет выдавать название по полю name таблицы Product (история добавления товаров
     # в панеле admin)
--- testSite/testSite/urls.py
+++ testSite/testSite/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -7,5 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # http://127.0.0.1:8000/myapp/
-    path('myapp/', include('myapp.urls', 'myapp'))
+    path('myapp/', include('myapp.urls', namespace='myapp'))
 ]
+# Добавляем url с media/
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
